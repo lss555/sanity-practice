b1ed67ffc3389be3682be38f7a29aa473e949fa1
lss555 <chrisheibel7@gmail.com>
2022-10-02 01:46:54 -0600

    Initial commit

---
+++ src/components/AllPosts.js
@@ -0,0 +1,54 @@
+// src/components/AllPosts.js
+
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
+
+export default function AllPosts() {
+  const [allPostsData, setAllPosts] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+          _id,
+          url
+        }
+      }
+    }`
+      )
+      .then((data) => setAllPosts(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h2>Blog Posts</h2>
+      <h3>Welcome to my blog posts page!</h3>
+      <div>
+        {allPostsData &&
+          allPostsData.map((post, index) => (
+            <Link to={"/" + post.slug.current} key={post.slug.current}>
+              <span key={index}>
+                
+                <img src={urlFor(post.mainImage.asset.url).width(200).url()} alt="" />
+                <span>
+                  <h2>{post.title}</h2>
+                </span>
+              </span>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
