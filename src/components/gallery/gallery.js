d9eb4239f75a9d4f17caaf19712e751ccfe14474
lss555 <chrisheibel7@gmail.com>
2022-10-18 21:35:10 -0600

    Animation work commented out

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -3,6 +3,9 @@ import sanityClient from "../../client.js";
 import imageUrlBuilder from "@sanity/image-url";
 import { BlogCards, BlogCardsContainer, BlogCardTitle, GalleryContainer, BlogLink, LoaderContainer } from './gallery-styles.js';
 import RingLoader from "react-spinners/RingLoader";
+// import { useInView } from 'react-intersection-observer';
+// import { motion } from 'framer-motion';
+// import { useAnimation } from 'framer-motion';
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -12,6 +15,24 @@ function urlFor(source) {
 const Gallery = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [allPostsData, setAllPosts] = useState(null);
+  // const { ref, inView } = useInView({
+  //   threshold: 0.05
+  // });
+  // const animation = useAnimation();
+
+  // useEffect(() => {
+  //   if(inView) {
+  //     animation.start({
+  //       x: 0,
+  //       transition: {
+  //         type: '', duration: 1, bounce: 0.3
+  //       }
+  //     });
+  //   }
+  //   if(!inView) {
+  //       animation.start({ x: '-100vw' })
+  //     }
+  // }, [inView]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -33,7 +54,8 @@ const Gallery = () => {
       .catch(console.error);
   }, []);
 
-
+// inside gallery container ref={ref}
+// around BlogLink <motion.div animate={animation} > </motion.div>
 
   return (
     <>
@@ -42,15 +64,18 @@ const Gallery = () => {
           <RingLoader />
         </LoaderContainer>
         ) : (
-        <GalleryContainer>
+        <GalleryContainer >
       {allPostsData &&
         allPostsData.map((post, index) => (
           <BlogCardsContainer key={post.slug.current}>
-            <BlogLink to={"/" + post.slug.current}>
-              <BlogCards src={urlFor(post.mainImage.asset.url).url()} alt={post.title} />
-              <BlogCardTitle>{post.title}</BlogCardTitle>
-            </BlogLink>
+            
+              <BlogLink to={"/" + post.slug.current}>
+                <BlogCards src={urlFor(post.mainImage.asset.url).url()} alt={post.title} />
+                <BlogCardTitle>{post.title}</BlogCardTitle>
+              </BlogLink>
+            
           </BlogCardsContainer>
+          
         ))}
       </GalleryContainer>
         )}
